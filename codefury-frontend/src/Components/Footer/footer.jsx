51728a37f7dbcc7a7d49d8fe3d158424ef21c0cd
Sinchana1706 <sinchana17062002@gmail.com>
2022-08-13 16:16:54 +0530

    footer added

---
+++ codefury-frontend/src/Components/Footer/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <div class="wrapper">
+            <div id="footer-info">
+                <p>Copyright 2014 Amplify . All rights reserved.</p>
+                <p><a href="#">Terms of Service</a> I <a href="#">Privacy</a></p>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </div>
+  )
+}
