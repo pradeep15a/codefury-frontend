e8725e79a90ebe9d4cefd648c4b274c5c21a441f
PRADEEP  A <pradeepashok1122@gmail.com>
2022-08-13 19:24:47 +0530

    homescreen made responsive

--- codefury-frontend/src/Components/Footer/footer.jsx
+++ codefury-frontend/src/Components/Footer/footer.jsx
@@ -3,14 +3,11 @@ import './footer.css';
 
 export default function Footer() {
   return (
-    <div>
+    <div className="footer">
         <div class="wrapper">
-            <div id="footer-info">
-                <p>Copyright 2014 Amplify . All rights reserved.</p>
+                <p>Copyright 2014 Amplify . All rights reserved.</p> 
                 <p><a href="#">Terms of Service</a> I <a href="#">Privacy</a></p>
             </div>
-            <div class="clear"></div>
-        </div>
     </div>
   )
 }
--- codefury-frontend/src/Components/Home/Home.jsx
+++ codefury-frontend/src/Components/Home/Home.jsx
@@ -17,64 +17,74 @@ const Home = () => {
         navigate('/about')
     }
 
-  return (
-    <div><div class="header">
-        <Header 
-            heading="Amplify"
-            details="adding profit to the lifes."
-        /></div>
-        <div id="primary-content">
-        <div class="wrapper">
-            <article>
-                <h3>Sales</h3>
-                <p>The program focuses on helping startups increase their sales, expand their customers, and improve their overall sales performance. 
-                    Being a business development service provider, -------- can help in finding sales reps for small businesses. In our Grow your Startup program, we will provide you with skilled sales representatives who can be a contact agent between your business and customers.
+    return (
+        <div className='main-container'>
+            <Header
+                heading="Amplify"
+                details="adding profit to the lifes."
+            />
+            <div id="primary-content">
+                <div class="wrapper">
+                    <article>
+                        <h3>Sales</h3>
+                        <div className="matter">
 
-Since --------is an online incubator, we have a worldwide network of sales reps who will help you in increasing your sales and revenues.
+                            <p>The program focuses on helping startups increase their sales, expand their customers, and improve their overall sales performance.
+                                Being a business development service provider, -------- can help in finding sales reps for small businesses. In our Grow your Startup program, we will provide you with skilled sales representatives who can be a contact agent between your business and customers.
 
-Our sales reps are provided with the best sales strategies that will help your products reach more target customers through online research, social media messaging, and email reach out. All these provided as a part of our business development services
-                </p>
-                <img src={f1} alt="video placeholder" />
-            </article>
-        </div>
-    </div>
-    <div id="primary-content">
-        <div class="wrapper">
-            <article>
-                <h3>Growth & Guidance</h3>
-                <p>"Becoming isn't about arriving somewhere or achieving a certain aim. I see it instead as forward motion, a means of evolving, a way to reach continuously toward a better self. The journey doesn't end." — Michelle Obama .
-                    The program helps you achieve growth for your startup through expanding into new global markets and exploring new sales channels and partnerships </p>
-                <img src={g2} alt="video placeholder" />
-            </article>
-        </div>
-    </div>
-    <div id="primary-content">
-        <div class="wrapper">
-            <article>
-                <h3>Funding</h3>
-                <p>This service is part of our Raise Capital program. We help small businesses and startups in getting the funding they need. We work with you from the very first steps and provide you with all the support and resources you need. We work with startups and investors on a global level and across all industries.
-                    Small businesses funding usually revolves around bank loans, SBA loans and small business grants as most small businesses usually have a good track record and significant revenue, which makes most small businesses eligible for loan funding.
-                </p> 
-           
-                <img src={support} alt="video placeholder" />
-            </article>
-        </div>
-    </div>
-    
+                                Since --------is an online incubator, we have a worldwide network of sales reps who will help you in increasing your sales and revenues.
 
-    
-    <div id="cta">
-        <div class="wrapper">
-            <h3>Heard Enough?</h3>
-            <p> Let’s Talk.</p>
+                                Our sales reps are provided with the best sales strategies that will help your products reach more target customers through online research, social media messaging, and email reach out. All these provided as a part of our business development services
+                            </p>
+                            <img src={f1} alt="video placeholder" />
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div id="primary-content">
+                <div class="wrapper">
+                    <article>
+                        <h3>Growth & Guidance</h3>
+                        <div className="matter">
+
+                            <p>"Becoming isn't about arriving somewhere or achieving a certain aim. I see it instead as forward motion, a means of evolving, a way to reach continuously toward a better self. The journey doesn't end." — Michelle Obama .
+                                The program helps you achieve growth for your startup through expanding into new global markets and exploring new sales channels and partnerships </p>
+                            <img src={g2} alt="video placeholder" />
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div id="primary-content">
+                <div class="wrapper">
+                    <article>
+                        <h3>Funding</h3>
+                        <div className="matter">
+
+                            <p>This service is part of our Raise Capital program. We help small businesses and startups in getting the funding they need. We work with you from the very first steps and provide you with all the support and resources you need. We work with startups and investors on a global level and across all industries.
+                                Small businesses funding usually revolves around bank loans, SBA loans and small business grants as most small businesses usually have a good track record and significant revenue, which makes most small businesses eligible for loan funding.
+                            </p>
+
+                            <img src={support} alt="video placeholder" />
+                        </div>
+                    </article>
+                </div>
+            </div>
+
+
+
+            <div id="cta">
+                <h3>Heard Enough?</h3>
+                <p> Let’s Talk.</p>
                 <p>Ready to see what Guidance can do for your business? Contact us now. We'll be in touch with you shortly </p>
-            <button onClick={handleProjects} class="button-2">Present your ideas</button>  <button onClick={handleInvest} class="button-2">Start investing</button>
-        </div>
-    </div>
+                <div className="btn">
+                    <button onClick={handleProjects} class="button-2">Present your ideas</button>
+                    <button onClick={handleInvest} class="button-2">Start investing</button>
+                </div>
+            </div>
 
-    <Footer />
-    </div>
-  )
+            <Footer />
+        </div>
+    )
 }
 
 export default Home;
